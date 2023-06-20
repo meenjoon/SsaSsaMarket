ef18ca2716deadee9e606697bdc97daf87a25d8c
meenjoon <zqudwnsz@naver.com>
2023-06-20 12:56:01 +0900

    feat: LogInFragment 화면 레이아웃 구성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     //Jetpack Navigation Graph 관련
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    //Google Play Services의 base 관련
+    implementation("com.google.android.gms:play-services-base:18.2.0")
 }
