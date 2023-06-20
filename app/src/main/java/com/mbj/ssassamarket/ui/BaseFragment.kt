306bf0f6e0a20fd739f372e9002f26f7e5ed4ff8
meenjoon <zqudwnsz@naver.com>
2023-06-20 12:14:49 +0900

    feat: 바인딩 클래스 연결 및 해제를 담당하는 BaseFragment 생성

---
+++ app/src/main/java/com/mbj/ssassamarket/ui/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.mbj.ssassamarket.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    private val binding: VB
+        get() = _binding!!
+
+    abstract fun inflateBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = inflateBinding(inflater, container)
+        return binding.root
+    }
+
+    final override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
