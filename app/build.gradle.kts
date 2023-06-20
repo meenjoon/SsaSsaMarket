823566c718148554e3c4d2e02b41ec4090d08cbe
meenjoon <zqudwnsz@naver.com>
2023-06-20 16:04:06 +0900

    chore: Safe args 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id ("androidx.navigation.safeargs.kotlin")
 }
 
 val properties = Properties()
