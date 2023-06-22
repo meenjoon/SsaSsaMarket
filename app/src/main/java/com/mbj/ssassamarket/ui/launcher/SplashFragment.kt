36a22dccb422a3f6358c0f1c994f27d1044a9a30
meenjoon <zqudwnsz@naver.com>
2023-06-22 14:50:35 +0900

    feat: SplashFragment 생성

---
+++ app/src/main/java/com/mbj/ssassamarket/ui/launcher/SplashFragment.kt
@@ -0,0 +1,11 @@
+package com.mbj.ssassamarket.ui.launcher
+
+import com.mbj.ssassamarket.R
+import com.mbj.ssassamarket.databinding.FragmentSplashFragementBinding
+import com.mbj.ssassamarket.ui.BaseFragment
+
+class SplashFragment : BaseFragment() {
+
+    override val binding get() = _binding as FragmentSplashFragementBinding
+    override val layoutId: Int get() = R.layout.fragment_splash_fragement
+}
