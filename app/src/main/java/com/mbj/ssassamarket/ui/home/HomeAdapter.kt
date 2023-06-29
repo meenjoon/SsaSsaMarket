0c44c42dccb60540e00832d3c21b15a451dcc007
meenjoon <zqudwnsz@naver.com>
2023-06-29 19:05:04 +0900

    feat: HomeAdapter를 구현하여 Home 화면을 구성

---
+++ app/src/main/java/com/mbj/ssassamarket/ui/home/HomeAdapter.kt
@@ -0,0 +1,87 @@
+package com.mbj.ssassamarket.ui.home
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.google.firebase.storage.FirebaseStorage
+import com.mbj.ssassamarket.R
+import com.mbj.ssassamarket.data.model.ProductPostItem
+import com.mbj.ssassamarket.databinding.RecyclerviewItemHomeProductBinding
+import com.mbj.ssassamarket.util.DateFormat.getFormattedElapsedTime
+
+class HomeAdapter : ListAdapter<ProductPostItem, HomeAdapter.HomeViewHolder>(homeDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        return HomeViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    class HomeViewHolder(val binding: RecyclerviewItemHomeProductBinding) : ViewHolder(binding.root) {
+
+        fun bind(productPostItem: ProductPostItem) {
+            loadFirstImage(productPostItem)
+            binding.homeProductTitleTv.text = productPostItem.title
+            binding.homeProductTimeTv.text = getFormattedElapsedTime(productPostItem.createdDate)
+            binding.homeProductPriceTv.text = productPostItem.price
+            binding.homeProductLocation.text = productPostItem.location
+        }
+
+        private fun loadFirstImage(postItem: ProductPostItem) {
+            val firstImageLocation = postItem.imageLocations?.firstOrNull()
+            val storageRef = FirebaseStorage.getInstance().reference
+            if (firstImageLocation != null) {
+                val imageRef = storageRef.child(firstImageLocation)
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    val roundedCorners = RoundedCornersTransformation(15f)
+                    binding.homeProductIv.load(uri.toString()) {
+                        transformations(roundedCorners)
+                        error(R.drawable.null_icon)
+                    }
+                }.addOnFailureListener { exception ->
+                    Log.e("HomeAdapter", "Failed to load image: ${exception.message}")
+                }
+            } else {
+                binding.homeProductIv.load(R.drawable.null_icon)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): HomeViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                return HomeViewHolder(
+                    RecyclerviewItemHomeProductBinding.inflate(
+                        inflater,
+                        parent,
+                        false
+                    )
+                )
+            }
+        }
+    }
+
+    companion object {
+        private val homeDiffCallback = object : DiffUtil.ItemCallback<ProductPostItem>() {
+            override fun areItemsTheSame(
+                oldItem: ProductPostItem,
+                newItem: ProductPostItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ProductPostItem,
+                newItem: ProductPostItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
--- app/src/main/java/com/mbj/ssassamarket/ui/home/HomeProductFragment.kt
+++ app/src/main/java/com/mbj/ssassamarket/ui/home/HomeProductFragment.kt
@@ -1,16 +1,43 @@
 package com.mbj.ssassamarket.ui.home
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.mbj.ssassamarket.R
 import com.mbj.ssassamarket.data.model.Category
+import com.mbj.ssassamarket.data.source.ProductRepository
 import com.mbj.ssassamarket.databinding.FragmentHomeProductBinding
 import com.mbj.ssassamarket.ui.BaseFragment
 import com.mbj.ssassamarket.util.Constants.KEY_HOME_PRODUCT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeProductFragment : BaseFragment() {
 
     override val binding get() = _binding as FragmentHomeProductBinding
     override val layoutId: Int get() = R.layout.fragment_home_product
+
+    @Inject
+    lateinit var productRepository: ProductRepository
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        lifecycleScope.launch {
+            val category = arguments?.getSerializable(KEY_HOME_PRODUCT) as? Category
+            if (category != null) {
+                val adapter = HomeAdapter()
+                binding.homeProductRv.adapter = adapter
+                val productPostItemList = productRepository.getProductByCategory(category)
+                adapter.submitList(productPostItemList)
+            }
+        }
+    }
 
     companion object {
         fun newInstance(category: Category) = HomeProductFragment().apply {
