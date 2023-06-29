7352c5fe899b7fcce5c07dcd67e192f986666116
meenjoon <zqudwnsz@naver.com>
2023-06-29 15:39:12 +0900

    feat: Hilt의 의존성 주입 설정된 부분 필요없어서 삭제

--- app/src/main/java/com/mbj/ssassamarket/data/source/remote/ApiClient.kt
+++ app/src/main/java/com/mbj/ssassamarket/data/source/remote/ApiClient.kt
@@ -38,30 +38,5 @@ interface ApiClient {
         @Path("dataId") dataId: String,
         @Body user: User
     ) : Response<Unit>
-
-    companion object {
-        private const val FIREBASE_BASE_URL = BuildConfig.FIREBASE_BASE_URL
-
-        fun create(): ApiClient {
-            val logger = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-
-            val moshi: Moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-
-            return Retrofit.Builder()
-                .baseUrl(FIREBASE_BASE_URL)
-                .client(client)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build()
-                .create(ApiClient::class.java)
-        }
-    }
 }
 
