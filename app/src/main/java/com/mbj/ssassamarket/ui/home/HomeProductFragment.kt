ce1f518ca9a8d490e6e35f61ccb3748c6d4a9b17
meenjoon <zqudwnsz@naver.com>
2023-06-29 15:47:23 +0900

    feat: ViewPager2와 TabLayout을 연동하여 홈 화면 설정

--- app/src/main/java/com/mbj/ssassamarket/ui/home/HomeProductFragment.kt
+++ app/src/main/java/com/mbj/ssassamarket/ui/home/HomeProductFragment.kt
@@ -1,11 +1,22 @@
 package com.mbj.ssassamarket.ui.home
 
+import android.os.Bundle
 import com.mbj.ssassamarket.R
+import com.mbj.ssassamarket.data.model.Category
 import com.mbj.ssassamarket.databinding.FragmentHomeProductBinding
 import com.mbj.ssassamarket.ui.BaseFragment
+import com.mbj.ssassamarket.util.Constants.KEY_HOME_PRODUCT
 
 class HomeProductFragment : BaseFragment() {
 
     override val binding get() = _binding as FragmentHomeProductBinding
     override val layoutId: Int get() = R.layout.fragment_home_product
+
+    companion object {
+        fun newInstance(category: Category) = HomeProductFragment().apply {
+            arguments = Bundle().apply {
+                putSerializable(KEY_HOME_PRODUCT, category)
+            }
+        }
+    }
 }
