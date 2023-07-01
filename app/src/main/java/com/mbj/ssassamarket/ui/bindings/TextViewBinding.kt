ba15e0ad3235ea9a3f126a90413fe193f2259734
meenjoon <zqudwnsz@naver.com>
2023-07-01 20:52:00 +0900

    feat: 언어별로 다르게 표현하는 경우, 문자열 리소스로 관리되도록 설정 함

--- app/src/main/java/com/mbj/ssassamarket/ui/bindings/TextViewBinding.kt
+++ app/src/main/java/com/mbj/ssassamarket/ui/bindings/TextViewBinding.kt
@@ -26,13 +26,21 @@ fun TextView.setFormattedElapsedTime(createdDate: String) {
     val diffInMonths = diffInDays / 30
     val diffInYears = diffInDays / 365
 
+    val context = this.context
+    val yearsAgo = context.getString(R.string.years_ago)
+    val monthsAgo = context.getString(R.string.months_ago)
+    val daysAgo = context.getString(R.string.days_ago)
+    val hoursAgo = context.getString(R.string.hours_ago)
+    val minutesAgo = context.getString(R.string.minutes_ago)
+    val justNow = context.getString(R.string.just_now)
+
     val result = when {
-        diffInYears > 0 -> "${diffInYears}년 전"
-        diffInMonths > 0 -> "${diffInMonths}달 전"
-        diffInDays > 0 -> "${diffInDays}일 전"
-        diffInHours > 0 -> "${diffInHours}시간 전"
-        diffInMinutes > 0 -> "${diffInMinutes}분 전"
-        else -> "방금 전"
+        diffInYears > 0 -> "$diffInYears$yearsAgo"
+        diffInMonths > 0 -> "$diffInMonths$monthsAgo"
+        diffInDays > 0 -> "$diffInDays$daysAgo"
+        diffInHours > 0 -> "$diffInHours$hoursAgo"
+        diffInMinutes > 0 -> "$diffInMinutes$minutesAgo"
+        else -> justNow
     }
 
     text = result
