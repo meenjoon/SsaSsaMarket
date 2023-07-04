93edb659eaf4b70383b3f573f7b763f413a42c2a
meenjoon <zqudwnsz@naver.com>
2023-07-05 01:23:43 +0900

    feat: 채팅 기능 확인을 위한 임시 BuyerFragment 생성

---
+++ app/src/main/java/com/mbj/ssassamarket/ui/detail/buyer/BuyerFragment.kt
@@ -0,0 +1,11 @@
+package com.mbj.ssassamarket.ui.detail.buyer
+
+import com.mbj.ssassamarket.R
+import com.mbj.ssassamarket.databinding.FragmentBuyerBinding
+import com.mbj.ssassamarket.ui.BaseFragment
+
+class BuyerFragment : BaseFragment() {
+    override val binding get() = _binding as FragmentBuyerBinding
+    override val layoutId: Int get() = R.layout.fragment_buyer
+
+}
