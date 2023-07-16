39a6c70de57b982af00592914674dc172fe3b224
meenjoon <zqudwnsz@naver.com>
2023-07-16 17:02:21 +0900

    feat: Flow 마이그레이션 - SplashFragment 화면

--- app/src/main/java/com/mbj/ssassamarket/ui/launcher/SplashFragment.kt
+++ app/src/main/java/com/mbj/ssassamarket/ui/launcher/SplashFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseUser
 import com.mbj.ssassamarket.R
 import com.mbj.ssassamarket.databinding.FragmentSplashFragementBinding
 import com.mbj.ssassamarket.ui.BaseFragment
-import com.mbj.ssassamarket.util.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -28,7 +30,22 @@ class SplashFragment : BaseFragment() {
         binding.viewModel = viewModel
         viewModel.checkCurrentUserExists()
 
-        observeAccountExists()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.isAccountExistsOnServer.collectLatest { isAccountExistsOnServer ->
+                        delay(2000)
+                        if (isAccountExistsOnServer != null) {
+                            navigateBasedOnUserState(
+                                viewModel.autoLoginState,
+                                isAccountExistsOnServer,
+                                viewModel.currentUser
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun navigateBasedOnUserState(
@@ -47,19 +64,6 @@ class SplashFragment : BaseFragment() {
             SplashFragmentDirections.actionSplashFragmentToLogInFragment()
         }
         findNavController().navigate(action)
-    }
-
-    private fun observeAccountExists() {
-        viewModel.isAccountExistsOnServer.observe(viewLifecycleOwner, EventObserver { isAccountExistsOnServer ->
-            lifecycleScope.launch {
-                delay(2000)
-                navigateBasedOnUserState(
-                    viewModel.autoLoginState,
-                    isAccountExistsOnServer,
-                    viewModel.currentUser
-                )
-            }
-        })
     }
 
     private fun showToast(messageResId: Int) {
--- app/src/main/java/com/mbj/ssassamarket/ui/launcher/SplashViewModel.kt
+++ app/src/main/java/com/mbj/ssassamarket/ui/launcher/SplashViewModel.kt
@@ -1,15 +1,12 @@
 package com.mbj.ssassamarket.ui.launcher
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.mbj.ssassamarket.data.model.User
 import com.mbj.ssassamarket.data.source.UserInfoRepository
 import com.mbj.ssassamarket.data.source.UserPreferenceRepository
 import com.mbj.ssassamarket.data.source.remote.network.ApiResultSuccess
-import com.mbj.ssassamarket.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,16 +14,25 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(private val repository: UserInfoRepository, private val userPreferenceRepository: UserPreferenceRepository) : ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val repository: UserInfoRepository,
+    private val userPreferenceRepository: UserPreferenceRepository
+) : ViewModel() {
 
-    private val _isAccountExistsOnServer = MutableLiveData<Event<Boolean>>()
-    val isAccountExistsOnServer: LiveData<Event<Boolean>> get() = _isAccountExistsOnServer
+    private val _isAccountExistsOnServer = MutableStateFlow<Boolean?>(null)
+    val isAccountExistsOnServer: StateFlow<Boolean?> = _isAccountExistsOnServer
 
     private val _isError = MutableStateFlow(false)
     val isError: StateFlow<Boolean> = _isError
 
     val autoLoginState = userPreferenceRepository.getSaveAutoLoginState()
-    val currentUser = FirebaseAuth.getInstance().currentUser
+    var currentUser: FirebaseUser? = null
+
+    init {
+        viewModelScope.launch {
+            currentUser = repository.getUserAndIdToken().first
+        }
+    }
 
     fun checkCurrentUserExists() {
         viewModelScope.launch {
@@ -37,14 +43,14 @@ class SplashViewModel @Inject constructor(private val repository: UserInfoReposi
                 if (response is ApiResultSuccess) {
                     val userMap = response.data
                     val isExists = isAccountExistsOnServer(userMap)
-                    _isAccountExistsOnServer.value = Event(isExists)
+                    _isAccountExistsOnServer.value = isExists
                 }
             }
         }
     }
 
     private suspend fun isAccountExistsOnServer(userMap: Map<String, Map<String, User>>): Boolean {
-        val uId = repository.getUserAndIdToken().first?.uid ?: ""
-        return userMap.containsKey(uId)
+        val uid = repository.getUserAndIdToken().first?.uid ?: ""
+        return userMap.containsKey(uid)
     }
 }
