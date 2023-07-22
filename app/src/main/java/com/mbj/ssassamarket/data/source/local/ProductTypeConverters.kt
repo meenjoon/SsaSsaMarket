7ff080402348dd17fa36b8df9fecf289a209ed1d
meenjoon <zqudwnsz@naver.com>
2023-07-22 23:04:46 +0900

    feat: ProductTypeConverters를 Moshi를 사용하여 JSON 직렬화/역직렬화하도록 변경

--- app/src/main/java/com/mbj/ssassamarket/data/source/local/ProductTypeConverters.kt
+++ app/src/main/java/com/mbj/ssassamarket/data/source/local/ProductTypeConverters.kt
@@ -1,16 +1,30 @@
 package com.mbj.ssassamarket.data.source.local
 
 import androidx.room.TypeConverter
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonDataException
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 
 class ProductTypeConverters {
 
+    private val moshi = Moshi.Builder().build()
+
     @TypeConverter
     fun fromStringList(list: List<String>?): String {
-        return list?.joinToString(",") ?: ""
+        val type = Types.newParameterizedType(List::class.java, String::class.java)
+        val jsonAdapter: JsonAdapter<List<String>> = moshi.adapter(type)
+        return jsonAdapter.toJson(list)
     }
 
     @TypeConverter
-    fun toStringList(value: String): List<String> {
-        return value.split(",")
+    fun toStringList(value: String): List<String>? {
+        val type = Types.newParameterizedType(List::class.java, String::class.java)
+        val jsonAdapter: JsonAdapter<List<String>> = moshi.adapter(type)
+        return try {
+            jsonAdapter.fromJson(value)
+        } catch (e: JsonDataException) {
+            null
+        }
     }
 }
